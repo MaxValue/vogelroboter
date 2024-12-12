5af8282552d6798b1f7f056d3f5562e4b649a625
Max Fuxjäger <maxvalue.git@gmail.com>
2024-12-12 22:32:35 +0100

    Fix mixup of width and height

--- quiz/static/main.js
+++ quiz/static/main.js
@@ -3,8 +3,8 @@
 function displayScreenSize() {
     console.log("handler running!")
     document.querySelectorAll(".screen-size-info").forEach(elem => {
-        elem.querySelector(".screen-size-info-width").innerText = window.screen.height;
-        elem.querySelector(".screen-size-info-height").innerText = window.screen.width;
+        elem.querySelector(".screen-size-info-width").innerText = window.screen.width;
+        elem.querySelector(".screen-size-info-height").innerText = window.screen.height;
     })
 };
 
